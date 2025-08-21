c1a1ef5756d48d4ffd67c40daa0cfa6e243bfbec
Elvis Brencis <elvis.brencis@gmail.com>
2025-08-21 18:30:22 +0000

    vai varam vēlreiz integrēt twilo sms sūtīšanas funkciju?

--- .next/static/chunks/src_app_place_confirm_page_tsx_d846e3da._.js
+++ .next/static/chunks/src_app_place_confirm_page_tsx_d846e3da._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_caff1847._.js"
+  "static/chunks/_30f293fa._.js"
 ],
     source: "dynamic"
 });
